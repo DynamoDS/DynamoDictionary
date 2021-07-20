f411e9331d15594119e7c2af22c7b071b28fd7d8
wmui51 <wmui51@users.noreply.github.com>
2021-07-20 04:10:17 -0700

    Chore/upgrade to react16 (#55)
    * chore: upgrade to React@16
    
    * chore: update package.json to React 16.14.0
    
    * chore: move react-scripts to fix npm audit
    
    https://github.com/facebook/create-react-app/issues/11081
    https://github.com/facebook/create-react-app/issues/11174
    
    * chore: remove deprecated react-tap-event-plugin
    
    * upgrade material-ui
    
    * add tooltip
    
    * update more mat-ui components
    
    * error check for add files

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,27 +1,26 @@
 import React from 'react';
-import IconButton from 'material-ui/IconButton';
+import IconButton from '@material-ui/core/IconButton';
 
-import ActionGrade from 'material-ui/svg-icons/action/grade';
-
+import ActionGrade from '@material-ui/icons/Grade';
 
 const IconButtonExampleTouch = () => (
   <div>
-    <IconButton tooltip="bottom-right" touch={true} tooltipPosition="bottom-right">
+    <IconButton tooltip="bottom-right" tooltipPosition="bottom-right">
       <img src="images/icons/pr_invert.png" id='prLogo' width= '40px' alt="prIcon" style={{'padding':'10px', 'paddingTop':'5px'}}/>
     </IconButton>
-    <IconButton tooltip="bottom-center" touch={true} tooltipPosition="bottom-center">
+    <IconButton tooltip="bottom-center" tooltipPosition="bottom-center">
       <ActionGrade />
     </IconButton>
-    <IconButton tooltip="bottom-left" touch={true} tooltipPosition="bottom-left">
+    <IconButton tooltip="bottom-left" tooltipPosition="bottom-left">
       <ActionGrade />
     </IconButton>
-    <IconButton tooltip="top-right" touch={true} tooltipPosition="top-right">
+    <IconButton tooltip="top-right" tooltipPosition="top-right">
       <ActionGrade />
     </IconButton>
-    <IconButton tooltip="top-center" touch={true} tooltipPosition="top-center">
+    <IconButton tooltip="top-center" tooltipPosition="top-center">
       <ActionGrade />
     </IconButton>
-    <IconButton tooltip="top-left" touch={true} tooltipPosition="top-left">
+    <IconButton tooltip="top-left" tooltipPosition="top-left">
       <ActionGrade />
     </IconButton>
   </div>
