f47a2059a3569f3d01a832928a96b64dfdeeb49d
Jorgen Dahl <jorgen.dahl@autodesk.com>
2022-10-19 12:57:02 -0400

    Remove promisify

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,10 +1,9 @@
 //import modules
 import * as d3 from "d3";
-import p from "../util/promise";
-//create promises for two data files to be read
-const pEdit = p.promisify(d3.json, "data/Dynamo_Nodes_Documentation.json");
-const pRevit = p.promisify(d3.xml, "data/Node_Library.xml");
-const pBacklog = p.promisify(d3.json, "data/Dynamo_Nodes_Revit.json");
+//create promises for the data files to be read
+const pEdit = d3.json("data/Dynamo_Nodes_Documentation.json");
+const pRevit = d3.xml("data/Node_Library.xml");
+const pBacklog = d3.json("data/Dynamo_Nodes_Revit.json");
 
 //resolve promises
 export default Promise.all([pRevit, pEdit, pBacklog]);
